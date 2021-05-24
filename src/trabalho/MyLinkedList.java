07bf7f104db17b66008503ca33c02ce1b6aa8b97
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-05-23 22:31:08 -0300

    MyLinkedList - 2019 version with updates

--- src/trabalho/MyLinkedList.java
+++ src/trabalho/MyLinkedList.java
@@ -2,325 +2,218 @@ package trabalho;
 
 /**
  * MyLinkedList.java
- * Implementação propria de lista encadeada genérica baseado em código de autoria da prof. Isabel Manssour.
- * @author Anderson Sprenger, Isabel Manssour
+ * Implementação propria de lista encadeada genérica.
+ *
  * @param <T> tipo dos elementos da lista.
+ * @author Anderson Sprenger
+ * @version 23 MAI 21
  */
-
 public class MyLinkedList<T> {
-
-    // Classe aninhada Node
-    private class Node {
-        public T element;
-        public Node next;
+    public class Node {
+        T element;
+        Node next = null;
 
         public Node(T element) {
             this.element = element;
-            next = null;
         }
-        
-        public Node(T element, Node next) {
-            this.element = element;
-            this.next = next;
-        }        
     }
 
-    
-    // Referência para o primeiro elemento da lista encadeada.
     private Node head;
-    // Referência para o último elemento da lista encadeada.
     private Node tail;
-    // Contador para a quantidade de elementos que a lista contem.
     private int count;
 
-    
-    /**
-     * Construtor da lista.
-     */
     public MyLinkedList() {
         head = null;
         tail = null;
         count = 0;
     }
-    
-    /**
-     * Retorna true se a lista nao contem elementos.
-     * @return true se a lista nao contem elementos
-     */
-    public boolean isEmpty() {
-        return (count==0); //(head == null);
-    }
 
-    /**
-     * Retorna o numero de elementos da lista.
-     * @return o numero de elementos da lista
-     */
     public int size() {
         return count;
     }
 
-    /**
-     * Esvazia a lista
-     */
     public void clear() {
         head = null;
         tail = null;
         count = 0;
-    }    
+    }
 
-    /**
-     * Adiciona um elemento ao final da lista.
-     * @param element elemento a ser adicionado ao final da lista
-     */
-    public void add(T element)  { // O(1)
-        Node n = new Node(element);
+    public void add(T element) {
+        Node e = new Node(element);
         if (head == null) {
-            head = n;
+            head = e;
         } else {
-            tail.next = n;
+            tail.next = e;
         }
-        tail = n;
+        tail = e;
         count++;
     }
-    
-    /**
-     * Retorna o elemento de uma determinada posição da lista.
-     * @param index a posição da lista
-     * @return o elemento da posição especificada
-     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
-     */
-    public T get(int index) { // O(n)
-        if ((index < 0) || (index >= count)) {
-            throw new IndexOutOfBoundsException(index < 0 ? "index < 0" : "index >= size()"); //erro
-        }
-        
-        // Verifica se o elemento buscado esta
-        // na ultima posição
-        if (index == count-1)
-            return tail.element; // retorna o elemento da ultima posição
-        Node aux = head; // referencia aux para percorrer a lista
-        int c = 0;
-        while (c < index) {
-            aux = aux.next;
-            c++;
-        }
-        return (aux.element);
-    }
 
-    /**
-     * Insere um elemento em uma determinada posição da lista.
-     * @param index a posição da lista onde o elemento sera inserido
-     * @param element elemento a ser inserido
-     * @throws IndexOutOfBoundsException se (index < 0 || index > size())
-     */
     public void add(int index, T element) {
-        // Primeiro verifica se index eh valido
-        if (index < 0 || index > size())
-            throw new IndexOutOfBoundsException(index < 0 ? "index < 0" : "index > size()"); // erro
-        
-        // Criar o node
-        Node n = new Node(element);
-        // Incrementar count
-        count++;
-        
-        // "Encadear" o nodo criado na lista
-        if (index == 0) { // Inserção no inicio
-            if (head==null) { // se a lista estiver vazia
-                tail = n;
+        if (index > size() || index < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == size()) {
+            add(element);
+        } else {
+            Node e = new Node(element);
+            if (index == 0) {
+                e.next = head;
+                head = e;
+            } else {
+                Node aux = head.next;
+                Node behindAux = head;
+                for (int i = 1; i < index; i++) {
+                    aux = aux.next;
+                    behindAux = behindAux.next;
+                }
+                e.next = aux;
+                behindAux.next = e;
+                count++;
             }
-            else { // se a lista não estiver vazia
-                n.next = head;
-            }
-            head = n;
         }
-        else if (index == count) { // Inserção no final
-            tail.next = n; // "conecta" o nodo na lista
-            tail = n; // atualiza tail
-        }
-        else { // Inserção no meio
-            // caminha ate a posição anterior
-            Node ant = head;
-            for (int i=0; i<index-1; i++)
-                ant = ant.next;
-            n.next = ant.next; // "conecta" o nodo na lista
-            ant.next = n; 
-        }
-        
     }
 
-    /**
-     * Substitui o elemento armazenado em uma determinada posição da lista pelo
-     * elemento indicado.
-     * @param index a posição da lista
-     * @param element o elemento a ser armazenado na lista
-     * @return o elemento armazenado anteriormente na posição da lista
-     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
-     */
-    public T set(int index, T element) {
-        // Primeiro verifica se index eh valido
-        if (index < 0 || index >= size())
-            throw new IndexOutOfBoundsException(index < 0 ? "index < 0" : "index >= size()");
-        
-        // Verifica se eh a ultima posição
-        if (index == count - 1) {
-            T rmElement = tail.element;
-            tail.element = element;
-            return rmElement;
+    public T get(int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (index >= size()) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == size() - 1) {
+            return tail.element;
         }
-                
-        // "caminha" ate a posição index
         Node aux = head;
-        for(int i=0; i < index; i++) {
+        for (int i = 0; i < index; i++) {
             aux = aux.next;
-        }        
-        // guarda o elemento que esta na posição index
-         T rmElement = aux.element;
-         
-        // coloca "element" na posição index
-         aux.element = element;
-         
-        // retorna o elemento guardado   
-        return rmElement;
+        }
+        return aux.element;
     }
 
-    /**
-     * Remove a primeira ocorrência do elemento na lista, se estiver presente.
-     * @param element o elemento a ser removido
-     * @return true se a lista contem o elemento especificado e ele eh removido
-     */
-    public boolean remove(T element) {
-        // retorna false se a lista estiver vazia
-        if (count == 0)
-            return false;
-        
-        if(head.element.equals(element)) {
-            head = head.next;
-            if (count==1) // se tinha apenas um elemento na lista
-                tail = null;
-            count--;
-            return true;
+    public T set(int index, T element) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == size() - 1) {
+            T elementRemoved = tail.element;
+            tail.element = element;
+            return elementRemoved;
+        } else {
+            Node aux = head;
+            for (int i = 0; i < index; i++) {
+                aux = aux.next;
+            }
+            T elementRemoved = aux.element;
+            aux.element = element;
+            return elementRemoved;
         }
-        Node aux = head.next;
-        Node ant = head;
-        
+    }
+
+    public T removeByIndex(int index) {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == 0) {
+            T elementRemoved = head.element;
+            head = head.next;
+            count--;
+            return elementRemoved;
+        } else {
+            Node aux = head;
+            for (int i = 1; i < index; i++) {
+                aux = aux.next;
+            }
+            T elementRemoved = aux.next.element;
+            aux.next = aux.next.next;
+            count--;
+            return elementRemoved;
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public boolean contains(T element) {
+        Node aux = head;
         while (aux != null) {
             if (aux.element.equals(element)) {
-                if (aux == tail) { // remoção do ultimo
-                    tail = ant;
-                    tail.next = null;
-                }
-                else { // remoção do meio
-                    ant.next = aux.next;
-                }                
-                count--;
                 return true;
             }
-            ant = ant.next;
             aux = aux.next;
         }
-        
         return false;
     }
 
-    /**
-     * Remove o elemento de uma determinada posição da lista.
-     * @param index a posição da lista
-     * @return o elemento que foi removido da lista
-     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
-     */
-    public T removeByIndex(int index) {
-        // Primeiro verifica se o índice eh valido
-        if (index < 0 || index >= size())
-            throw new IndexOutOfBoundsException(index < 0 ? "index < 0" : "index >= size()"); // erro
-        
-        // Remoção do primeiro elemento da lista
-        if(index == 0) {
-            T elem = head.element; // guarda o elemento que sera removido
-            head = head.next;
-            if (count==1) // se tinha apenas um elemento na lista
-                tail = null;
-            count--;
-            return elem; // retorna o elemento removido
-        }
-        
-        // Remove do meio ou do final
-        
-        return null;
-    }
-
-    /**
-     * Retorna o índice da primeira ocorrência do elemento na lista, ou -1 se a
-     * lista nao contem o elemento.
-     * @param element o elemento a ser buscado
-     * @return o índice da primeira ocorrência do elemento na lista, ou -1 se a
-     * lista nao contem o elemento
-     */
     public int indexOf(T element) {
+        int index = 0;
         Node aux = head;
-        for (int i = 0; i < count; i++) {
+        while (aux != null) {
             if (aux.element.equals(element)) {
-                return i;
-            } else {
-                aux = aux.next;
+                return index;
             }
+            aux = aux.next;
+            index++;
         }
         return -1;
     }
 
-    /**
-     * Retorna true se a lista contem o elemento especificado.
-     * @param element o elemento a ser testado
-     * @return true se a lista contem o elemento especificado
-     */
-    public boolean contains(T element) {
-        Node aux = head;
-        for (int i = 0; i < count; i++) {
+    public boolean remove(T element) {
+        if (head.element.equals(element)) {
+            head = head.next;
+            count--;
+            return true;
+        }
+        Node behindAux = head;
+        Node aux = head.next;
+        while (aux != null) {
             if (aux.element.equals(element)) {
+                behindAux.next = aux.next;
+                count--;
                 return true;
             }
+            behindAux = behindAux.next;
             aux = aux.next;
         }
         return false;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder bodyBuilder = new StringBuilder();
-
+    public T[] toArray() {
+        Object[] array = new Object[size() - 1];
         Node aux = head;
-
-        while (aux != null) {
-            bodyBuilder.append("[");
-            bodyBuilder.append(aux.element);
-            bodyBuilder.append("] ");
+        for (int i = 0; i < size(); i++) {
+            array[i] = aux.element;
             aux = aux.next;
         }
-
-        return bodyBuilder.toString();
+        return (T[]) array;
     }
 
-    /**
-     * @return uma representação em String da lista com um elemento em destaque
-     * @param highlightedElement elemento em destaque na lista
-     */
-    public String toStringWithHighlightedElement(T highlightedElement) {
-        StringBuilder bodyBuilder = new StringBuilder();
-
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
         Node aux = head;
-
-        while (aux != null) {
-            if (aux.element.equals(highlightedElement)) {
-                bodyBuilder.append("[");
-                bodyBuilder.append(aux.element);
-                bodyBuilder.append("] ");
-            } else {
-                bodyBuilder.append(aux.element);
-                bodyBuilder.append(" ");
-            }
-
+        str.append("[");
+        for (int i = 0; i < size(); i++) {
+            str.append(aux.element);
+            str.append(", ");
             aux = aux.next;
         }
+        str.deleteCharAt(str.length() - 1);
+        str.deleteCharAt(str.length() - 1);
+        str.append("]");
+        return str.toString();
+    }
 
-        return bodyBuilder.toString();
+    public String toStringWithHighlightedElement(T element) {
+        StringBuilder str = new StringBuilder();
+        Node aux = head;
+        for (int i = 0; i < size(); i++) {
+            if (aux.element.equals(element)) {
+                str.append("[");
+                str.append(aux.element);
+                str.append("] ");
+            } else {
+                str.append(aux.element);
+                str.append(" ");
+            }
+            aux = aux.next;
+        }
+        return str.toString();
     }
 }
