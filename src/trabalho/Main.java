6b72d8d9d13b02e2b2fff1f43d13966cd22657a5
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-05-25 16:07:19 -0300

    Main - add interface textual que permita fazer ciranda mais de uma vez (até que o usuário não queira mais)

--- src/trabalho/Main.java
+++ src/trabalho/Main.java
@@ -1,9 +1,24 @@
 package trabalho;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         MagicCircle magicCircle = new MagicCircle();
-        magicCircle.startCeremony(20);
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Circulo Mágico de Mármia - com 20 habitantes:");
+        magicCircle.startCeremony(20); // Complexidade: O(n) = n^2
+        System.out.println();
+
+        while (true) {
+            System.out.println("Digite o tamanho da população, ou 0 para sair:");
+            int populationSize = scn.nextInt();
+            if (populationSize <= 0)
+                return;
+            magicCircle = new MagicCircle();
+            magicCircle.startCeremony(populationSize);
+        }
     }
 }
